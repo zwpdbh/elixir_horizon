476b3c18c9fdba4e024b0757af6aa4700053134a
zwpdbh <zw>
2024-05-31 09:47:16 +0800

    I need static type

--- apps/elixir_in_action/lib/how/playground.ex
+++ apps/elixir_in_action/lib/how/playground.ex
@@ -13,3 +13,25 @@ defmodule How.Playground do
     end
   end
 end
+
+defmodule Person do
+  defstruct name: "unknow", age: 0, email: nil
+end
+
+defmodule MyApi do
+  def do_something(%Person{name: "wei", age: 0}) do
+    {:error, "no email"}
+  end
+
+  def do_something(%Person{email: email}) do
+    {:ok, email}
+  end
+
+  def test_email() do
+    with {:ok, email} <- do_something(%Person{name: "wei"}) do
+      {:ok!, email}
+    else
+      e -> e
+    end
+  end
+end
